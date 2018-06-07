f98b2110060cba221fc46e6d1e74bef3435d6479
thetylerwolf <tylernicholaswolf@gmail.com>
2018-06-07 10:17:30 -0400

    small fixes

--- src/Year.js
+++ src/Year.js
@@ -22,7 +22,8 @@ class Year extends Component {
             'width':'100%'
           }}
           height="100%"
-          width="100%">
+          width="100%"
+          allowFullScreen="true">
         </iframe>
 
         <footer>
